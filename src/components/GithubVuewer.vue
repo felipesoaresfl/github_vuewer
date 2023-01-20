d8d7d19306ab8907883a2c4637e557de0664bb49
Felipe Soares <felipe.soares013@outlook.com>
2023-01-20 10:15:16 -0300

    cria component githubvuewer

---
+++ src/components/GithubVuewer.vue
@@ -0,0 +1,21 @@
+<template>
+    <v-container>
+      <v-row class="text-center">
+        <v-col cols="12">
+          <GithubRepos/>
+        </v-col>
+      </v-row>
+    </v-container>
+</template>
+
+<script>
+import GithubRepos from './GithubRepos';
+export default {
+    components: {
+      GithubRepos,
+    },
+    data: () => ({
+
+    }),
+}
+</script>
