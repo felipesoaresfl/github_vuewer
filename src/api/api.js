a5f312fbb0361fd8930fe5f11f1b2338bbe53cac
Felipe Soares <felipe.soares013@outlook.com>
2023-01-23 20:03:05 -0300

    fix pagination

--- src/api/api.js
+++ src/api/api.js
@@ -1,12 +1,29 @@
+async function fetch_all_pages(url){
+	let nextPage = true
+	let result = []
+	let page = 1
+	while(nextPage){
+			const response = await fetch(`${url}?page=${page}`)
+			const tmpitems = await response.json()
+			if(tmpitems.length > 0){
+					result = result.concat(tmpitems)
+					page++
+			} else {
+					nextPage = false
+			}
+	}
+	return result
+}
+
 export const api = {
   async search_users(searchstring){
 		const url = `https://api.github.com/search/users?q=${searchstring}`
-    const result = await fetch(url)
-		return await result.json()
+    const response = await fetch(url)
+		return await response.json()
   },
 	async list_repos(username){
 		const url = `https://api.github.com/users/${username}/repos`
-		const result = await fetch(url)
-    return await result.json()
+		const data = await fetch_all_pages(url)
+    return await data
 	}
 }
