6539adb6c3d3ce533d96afc112834978f42c58db
Felipe Soares <felipe.soares013@outlook.com>
2023-01-25 20:14:49 -0300

    add component githubfiles uhul

--- src/components/GithubFiles.vue
+++ src/components/GithubFiles.vue
@@ -2,17 +2,39 @@
   <v-container>
     <v-row>
       <v-col cols="12">
+				<v-banner
+					v-if="actualPath"
+				>
+					📂 {{actualPath}}
+				</v-banner>
         <v-simple-table>
           <template v-slot:default>
             <thead>
               <tr>
-                <th class="text-left">Docs</th>
+                <th class="text-left">Documents</th>
               </tr>
             </thead>
             <tbody>
-              <tr v-for="doc in docs" :key="doc.url">
-                <td>{{ doc.name }}</td>
-              </tr>
+							<tr v-for="content in contents" :key="content.name">
+								<td v-if="isDirectory(content.type)">
+                  <v-icon class="icon">mdi-folder</v-icon>
+                  <button
+                    class="directory teal--text text-decoration-underline"
+                    @click="openDirectory(content.path)"
+                  >
+                    {{ content.name }}
+                  </button>
+								</td>
+								<td v-else>
+                  <v-icon class="icon">mdi-file-outline</v-icon>
+                  {{ content.name }}
+                </td>
+							</tr>
+							<div v-if="typeof previousPath == 'string'">
+								<v-btn class="ma-2" outlined color="teal" @click="goBack">
+                  Back
+								</v-btn>
+							</div>
             </tbody>
           </template>
         </v-simple-table>
@@ -33,24 +55,56 @@ import { api } from '~api'
 export default {
 	props: ['repo'],
 	data: () => ({
-		docs: [],
+		contents: [],
 		loading: false,
+    directoryContent: true,
+		previousPath: null,
+		actualPath: null,
 	}),
 	methods: {
-		async getFiles(){
+		async listContent(){
 			this.loading = true
-			const files = await api.get_files(this.repo.owner.login, this.repo.name)
-		this.docs = this.docs.concat(files)
+			const contents = await api.get_files(this.repo.owner.login, this.repo.name)
+			this.contents = this.contents.concat(contents)
 			this.loading = false
-		}
+		},
+		async listFolderContent(path) {
+			let gitPath = []
+      this.loading = true;
+      const contents = await api.list_folder_content(this.repo.owner.login, this.repo.name, path);
+			gitPath.push(this.repo.owner.login, this.repo.name, path)
+      this.actualPath = gitPath.join('/')
+			
+      let newPreviousPathList = path.split("/");
+      newPreviousPathList.pop();
+      const newPreviousPath = newPreviousPathList.join("/");
+      this.previousPath = newPreviousPath;
+      this.contents = this.contents.concat(contents);
+      this.loading = false;
+    },
+    isDirectory(type) {
+      return type == "dir";
+    },
+    openDirectory(path) {
+      this.contents = [];
+      this.listFolderContent(path);
+    },
+    goBack() {
+      if (this.previousPath == "") {
+        this.contents = [];
+        this.listContent();
+      } else {
+        this.contents = [];
+        this.listFolderContent(this.previousPath);
+      }
+    },
 	},
 	watch: {
 		repo(){
-			this.docs = []
-			if(this.repo){
-				this.getFiles()
-			}
+			this.contents = []
+			this.listContent()
 		}
 	}
 }
+
  </script>
